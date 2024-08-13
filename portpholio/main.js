0d2b66f3acb774f7bc801b77bc23489b59cbc303
Chirag Bharda <160694953+chiragbharda@users.noreply.github.com>
2024-08-13 20:11:06 +0530

    Add files via upload

---
+++ portpholio/main.js
@@ -0,0 +1,5 @@
+<><script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script><script>
+    var typed = new Typed('#element', {strings}: ['Web Developer', 'Video Editor', 'Web Designer', 'Graphics Designer',],
+    typeSpeed: 50,
+    {"}"});
+</script></>
